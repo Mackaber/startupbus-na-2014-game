62d5592fe46b9d6b67ea6adf7a8e1c4746991d57
Blane Cordes <blane.cordes@echostar.com>
2014-02-25 18:23:48 -0500

    welcome email

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -1,10 +1,10 @@
 class Notifier < ActionMailer::Base
-  default :from => 'blane.cordes@gmail.com'
+  default :from => 'buspreneur2014@startupbus.com'
 
   # send a signup email to the user, pass in the user object that   contains the user's email address
   def send_signup_email(user)
     @user = user
     mail( :to => @user.email,
-    :subject => 'Thanks for signing up for our amazing app' )
+    :subject => 'Welcome To The Start Up Bus Game!')
   end
 end
--- config/environment.rb
+++ config/environment.rb
@@ -6,11 +6,10 @@ DeviseApp::Application.initialize!
 
 
 ActionMailer::Base.smtp_settings = {
-  :user_name => 'bc00',
-  :password => 'test1234',
-  :domain => 'http://localhost:3000/',
-  :address => 'smtp.sendgrid.net',
-  :port => 587,
+  :address => "smtp.sendgrid.net",
+  :port => 25,
+  :domain => "game.startupbus.com",
   :authentication => :plain,
-  :enable_starttls_auto => true
+  :user_name => ENV['SENDGRID_USERNAME'],
+  :password => ENV['SENDGRID_PASSWORD']
 }
