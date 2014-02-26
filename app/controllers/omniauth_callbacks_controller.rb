8d591680220de6e23e10dcc2ede78162243da2d6
Jon Gottfried <jonmarkgo@gmail.com>
2014-02-26 15:13:15 -0500

    Check buspreneur approval via WordPress API

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,10 +1,14 @@
+require 'net/http'
+require 'net/https'
+require 'json'
+
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def all
     # raise request.env["omniauth.auth"].to_yaml
     omniauthable = Omniauthable.from_omniauth(request.env["omniauth.auth"])
 
     if omniauthable.persisted?
-      flash.notice = "Signed In, Bro!"
+      flash.notice = "Signed In!"
       sign_in_and_redirect omniauthable
       check_buspreneur_approval(current_user)
     else
@@ -31,9 +35,22 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   private
 
   def check_buspreneur_approval(current_user)
-    if ENV["BUSPRENEUR_EMAIL_ADDRESSES"].include?(current_user.email) && current_user.approved_at.nil?
-      current_user.approved_at = Time.now
-      current_user.save!
+    http = Net::HTTP.new("northamerica.startupbus.com",443)
+    req = Net::HTTP::Get.new("/index.php?__api=1&buspreneurs=#{current_user.email}")
+    http.use_ssl = true
+
+    req.basic_auth ENV["STARTUPBUS_WP_USERNAME"], ENV["STARTUPBUS_WP_PASS"]
+    begin
+      response = http.request(req)
+      if !response.kind_of? Net::HTTPNotFound
+        buspreneur = JSON.parse(response.body)
+        if buspreneur['email'] == current_user.email && current_user.approved_at.nil?
+          current_user.approved_at = Time.now
+          current_user.save!
+        end
+      end
+    rescue Exception => e
+      puts "Error requesting buspreneur approval: #{e.inspect}"
     end
   end
   
