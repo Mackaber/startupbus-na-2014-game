8cbe0c2cc348f9d015ac77d1ccc2e1963f67e22a
Blane Cordes <blane.cordes@echostar.com>
2014-02-26 23:56:20 -0500

    add validation to make sure conductors dont award too many points for milestone

--- app/models/milestone_team.rb
+++ app/models/milestone_team.rb
@@ -4,4 +4,11 @@ class MilestoneTeam < ActiveRecord::Base
 
   validates_uniqueness_of :team_id, scope: [:milestone_id]
 
+  validate :awarded_points_must_be_less_than_max_points
+ 
+  def awarded_points_must_be_less_than_max_points
+    if awarded_points > milestone.max_points
+      errors.add(:too_many_points_awarded, "Must be less than max points allowed for milestone.")
+    end
+  end
 end
