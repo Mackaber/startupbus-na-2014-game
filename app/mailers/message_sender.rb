08ac864bf97ca8c35677004272561c3ef6e74fb7
Jon Gottfried <jonmarkgo@gmail.com>
2014-02-28 15:54:59 -0500

    Track all conductor mail together, and don't show unsubscribe link

--- app/mailers/message_sender.rb
+++ app/mailers/message_sender.rb
@@ -1,9 +1,10 @@
 class MessageSender < ActionMailer::Base
   include SendGrid
-  sendgrid_category :use_subject_lines
-  sendgrid_enable   :ganalytics, :opentrack, :clicktrack
+  sendgrid_category "ConductorMail"
+
 
   def send_message_email(subject, body, conductor, buspreneur)
+    sendgrid_disable :subscriptiontrack
     mail( :from => conductor.email,
           :to => buspreneur.email,
           :subject => subject,
