82f57c300c3e6569097e3935f1943602e4a4c94e
Jon Gottfried <jonmarkgo@gmail.com>
2014-02-28 13:44:49 -0500

    Added Messages model

--- app/models/team.rb
+++ app/models/team.rb
@@ -6,6 +6,8 @@ class Team < ActiveRecord::Base
   has_many :milestone_teams
   has_many :milestones, :through => :milestone_teams
 
+  has_many :messages
+
   accepts_nested_attributes_for :milestone_teams
 
   belongs_to :bus
