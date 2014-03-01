cc7c90ab093d0cecd18aa6420000df922a75a9d6
Jon Gottfried <jonmarkgo@gmail.com>
2014-03-01 14:15:52 -0500

    Updated DB schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20140301173157) do
-
+ActiveRecord::Schema.define(version: 20140301175422) do
 
   create_table "buses", force: true do |t|
     t.string   "name"
