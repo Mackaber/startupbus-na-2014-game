db92a544c80fc1228da6c8e1ae82f4d54df84f88
Blane Cordes <blane.cordes@echostar.com>
2014-03-01 10:54:42 -0500

    spec

---
+++ spec/models/investor_game_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+describe InvestorGame do
+  let(:game) { InvestorGame.new }
+  let(:team) { InvestorGame.new }
+  let(:investor) { Investor.new }
+  let(:northeast_bus) { Bus.new }
+  let(:westcoast_bus) { Bus.new }
+
+  it "should assign a score to each team based on money invested and milestones completed" do 
+    
+  end
+
+  it "should reflect the number of people in the game to the investor" do 
+    
+  end
+
+  it "should reflect the time remaining to the investor" do 
+    
+  end
+
+end
