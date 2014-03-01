1e36bf489826203390bb539910f2301383c66ad3
Jon Gottfried <jonmarkgo@gmail.com>
2014-03-01 18:35:26 -0500

    HTML email, mofo

--- app/mailers/update_sender.rb
+++ app/mailers/update_sender.rb
@@ -2,11 +2,12 @@ class UpdateSender < ActionMailer::Base
   include SendGrid
   sendgrid_category "TeamUpdate"
 
-
   def send_update_email(subject, body, investor)
-    mail( :from => "game@startupbus.com",
+    @str = body
+    mail(:from => "game@startupbus.com",
           :to => investor.email,
-          :subject => subject,
-          :body => body)
+          :subject => subject) do |format|
+            format.html { render "notifier/update_email" }
+          end
   end
 end
