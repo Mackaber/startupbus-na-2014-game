b6372d891d4afbd6d019aaf451cb058f7cc4e26d
Jon Gottfried <jonmarkgo@gmail.com>
2014-03-01 12:07:47 -0500

    teams and milestones have many requests

--- app/models/team.rb
+++ app/models/team.rb
@@ -7,7 +7,7 @@ class Team < ActiveRecord::Base
   has_many :investments
   has_many :milestone_teams
   has_many :milestones, :through => :milestone_teams
-
+  has_many :team_milestone_requests
   has_many :messages, :through => :team_messages
 
   accepts_nested_attributes_for :milestone_teams
